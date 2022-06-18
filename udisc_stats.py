3b0488290d0cd7f7186028877e61b54796a20902
Justin Essert <justinkessert@gmail.com>
2022-06-18 14:21:48 -0400

    Added month plot and refreshed data

--- udisc_stats.py
+++ udisc_stats.py
@@ -14,11 +14,12 @@ def print_segments(df):
     print(f"Courses: {list(df.CourseName.unique())}")
     print(f"Layouts: {list(df.LayoutNameAdj.unique())}")
 
-def preprocess(df):
+def preprocess(df, drop_partial=True):
     df = df.copy()
     df.Date = pd.to_datetime(df.Date)
 
     min_pavers_date = df[df.LayoutName == "Paver Tees"].Date.min()
+    print(min_pavers_date)
     df["LayoutNameAdj"] = df.LayoutName
     df.loc[
         (df.CourseName == "Bryan Park") &
@@ -35,7 +36,13 @@ def preprocess(df):
 
     df['Year'] = pd.DatetimeIndex(df['Date']).year
     
-    return df
+    if drop_partial:
+        hole_cols = [x for x in df.columns if x.startswith("Hole")]
+        # Missing Scores represented as 0.0
+        for col in hole_cols:
+            df = df[df[col] != 0.0]
+    
+    return df.drop_duplicates().reset_index(drop=True)
 
 def get_year_stats(df):
     year_df = df.groupby(seg_cols + ["Year"]).mean().reset_index()
@@ -74,7 +81,7 @@ def get_score_avg(df, periods=[5, 10, 20]):
     
     return score_df
 
-def get_score_counts(df, period=10):
+def get_score_counts(df, period=10, holes=None):
     hole_cols = [x for x in df.columns if x.startswith("Hole")]
 
     melt_df = df.melt(id_vars=seg_cols + ["Date"], value_vars=hole_cols)
@@ -108,6 +115,9 @@ def get_score_counts(df, period=10):
     players_par_df.loc[players_par_df["Diff"] > 3, "ScoreName"] = "Triple Bogie+"
 
     assert len(players_par_df[players_par_df.Diff.isna()]) == 0
+    
+    if holes is not None:
+        players_par_df = players_par_df[players_par_df.Hole.isin(holes)]
 
     group_df = players_par_df[
         seg_cols + ["Date", "ScoreName", "Score"]
@@ -139,7 +149,42 @@ def get_score_counts(df, period=10):
     
     return ma_df
 
-def get_player_stats(df, player, course, layout):
+def get_month_df(df):
+    month_df = df.copy()
+    month_df["Month"] = (month_df['Date'].dt.floor("D") + pd.offsets.MonthBegin(-1))
+    month_df.rename(columns={"+/-": "Score", "Total": "Num Rounds"}, inplace=True)
+    month_agg_df = month_df.groupby(seg_cols + ["Month"]).agg({"Score": "mean", "Num Rounds": "count"}).reset_index()
+    
+    return month_agg_df
+
+def plot_month_df(viz_df, title):
+    fig, ax = plt.subplots(figsize=(15, 8))
+
+    viz_df["MonthStr"] = viz_df["Month"].dt.strftime("%Y-%m")
+
+    pastel_orange=(1.0, 0.7058823529411765, 0.5098039215686274)
+    sns.barplot(x="MonthStr", y="Num Rounds", color=pastel_orange, data=viz_df, ax=ax)
+    ax.grid(visible=True, axis="y", linestyle="--")
+    ax2 = ax.twinx()
+    sns.lineplot(x="MonthStr", y="Score", marker='o', data=viz_df, ax=ax2)
+    ax.set_title(title)
+
+    for x,y in zip(viz_df.MonthStr, viz_df.Score):
+
+        label = "{:.2f}".format(y)
+
+        ax2.annotate(label,
+                     (x,y),
+                     textcoords="offset points",
+                     xytext=(0,10),
+                     ha='center')
+
+def get_player_stats(df, player, course, layout, holes=None, min_date=None):
+    if min_date is None:
+        min_date = df.Date.min()
+    if isinstance(min_date, str):
+        min_date = pd.Timestamp(min_date)
+
     year_df = get_year_stats(df)
     print(f"Yearly Stats for {player} at {course} from the {layout}")
     display(year_df[
@@ -153,19 +198,33 @@ def get_player_stats(df, player, course, layout):
     viz_df = score_df[
         (score_df.PlayerName == player) &
         (score_df.CourseName == course) &
-        (score_df.LayoutNameAdj == layout)
+        (score_df.LayoutNameAdj == layout) &
+        (score_df.Date >= min_date)
     ]
     sns.lineplot(data=viz_df, x="Date", y="value", hue="variable", ax=ax).set(
         title=f"Score Relative to Par for {player} at {course} from the {layout}"
     )
 
-    score_counts_df = get_score_counts(df)
+    score_counts_df = get_score_counts(df, holes=holes)
     fig, ax = plt.subplots(figsize=(15, 8))
     viz_df = score_counts_df[
         (score_counts_df.PlayerName == player) &
         (score_counts_df.CourseName == course) &
-        (score_counts_df.LayoutNameAdj == layout)
+        (score_counts_df.LayoutNameAdj == layout) &
+        (score_counts_df.Date >= min_date)
     ]
     sns.lineplot(x="Date", y="Frequency", hue="ScoreName", data=viz_df).set(
         title=f"10 Round Avg of # of Each Score Achieved by {player} at {course} on the {layout}"
     )
+    
+    month_agg_df = get_month_df(df)
+    
+    viz_df = month_agg_df[
+        (month_agg_df.PlayerName == player) &
+        (month_agg_df.CourseName == course) &
+        (month_agg_df.LayoutNameAdj == layout) &
+        (month_agg_df.Month >= min_date)
+    ]
+    
+    title = f"Avg Score & Number of Rounds Played by {player} at {course} on the {layout}"
+    plot_month_df(viz_df, title)
